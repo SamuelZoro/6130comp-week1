257767a7fded317c710a167349854d1b419da60b
SamuelZoro <sam.l.zoro@live.com>
2023-01-21 21:30:09 +0000

    additional task

--- test.php
+++ test.php
@@ -7,6 +7,6 @@
     <title>Cloud week 1</title>
 </head>
 <body>
-<?php echo '<p>Hello from cloud week 1</p>';?>
+<?php echo '<p>Hello from cloud week 1</p>'; echo '<p>I have made a change as an additional task</p>'?>
 </body>
 </html>
